f4fb4762b493f0640ab51d37960c136cb3736b11
KimJiHun <kjh31021@g.skku.edu>
2024-07-30 16:17:19 +0900

    Add to he-conv1 layer

--- go/cnnLayer.go
+++ go/cnnLayer.go
@@ -5,6 +5,7 @@ import (
 	"sync"
 
 	"github.com/tuneinsight/lattigo/v5/core/rlwe"
+	"github.com/tuneinsight/lattigo/v5/he/hefloat"
 )
 
 type Conv1DLayer struct {
@@ -38,8 +39,11 @@ func HEConv1Layer(ctx *Context, op0 *Ciphertext, layer *Conv1DLayer) (opOut *Cip
 	space := op0.space
 	slots := ctx.params.MaxSlots()
 
+	numIn := len(op0.data)
+	numOut := int(math.Ceil(float64(space * chIn * chOut) / float64(slots)))
+
 	opOut = &Ciphertext{
-		data: 		make([]*rlwe.Ciphertext, chOut),
+		data: 		make([]*rlwe.Ciphertext, numOut),
 		size: 		(op0.size + 2*layer.Padding - layer.KernelSize) / layer.Stride + 1,
 		interval: 	op0.interval * layer.Stride,
 		constVal: 	1,
@@ -51,8 +55,8 @@ func HEConv1Layer(ctx *Context, op0 *Ciphertext, layer *Conv1DLayer) (opOut *Cip
 	}
 	op1 := ctx.fillHEData(op0)
 
-	cRot := make([][]*rlwe.Ciphertext, chIn)
-	for i := 0; i < chIn; i++ {
+	cRot := make([][]*rlwe.Ciphertext, numIn)
+	for i := 0; i < numIn; i++ {
 		cRot[i] = make([]*rlwe.Ciphertext, layer.KernelSize)
 	}
 
@@ -70,29 +74,92 @@ func HEConv1Layer(ctx *Context, op0 *Ciphertext, layer *Conv1DLayer) (opOut *Cip
 	}
 	wg.Wait()
 
-	numIn := len(op1.data)
-	numOut := int(math.Ceil(float64(space * chIn * chOut) / float64(slots)))
+	cOuts := make([][]*rlwe.Ciphertext, numOut)
+	for i := 0; i < numOut; i++ {
+		cOuts[i] = make([]*rlwe.Ciphertext, numIn * layer.KernelSize)
+	}
 
 	wg.Add(numIn * numOut * layer.KernelSize)
-	for o := 0; o < numOut; o++ {
-		for i := 0; i < numIn; i++ {
+	for no := 0; no < numOut; no++ {
+		for ni := 0; ni < numIn; ni++ {
 			for k := 0; k < layer.KernelSize; k++ {
-				go func(o, i, k int) {
+				go func(no, ni, k int) {
 					defer wg.Done()
-					// vKer := append(
-					// 	[]float64{layer.Weight[o][i][q]}
-					// )
-
-
-
-				}(o, i, k)
+					vKer := make([]float64, 0)
+					for o := 0; o < chOut / numOut; o++ {
+						outIdx := no * chOut / numOut + o
+						for i := 0; i < chIn / numIn; i++ {
+							inIdx := ni * chIn / numIn + i
+							
+							weight := []float64{layer.Weight[outIdx][inIdx][k]}
+							weight = append(weight, repeatZeros(opOut.interval - 1)...)
+							weight = repeatSlice(weight, opOut.size)
+							weight = append(weight, repeatZeros(space - len(weight))...)
+							vKer = append(vKer, weight...)
+						}
+					}
+					vKer = repeatSlice(vKer, slots / len(vKer))
+					
+					eval := ctx.evalPool.Get().(*hefloat.Evaluator)
+					ct, err := eval.MulRelinNew(cRot[ni][k], vKer)
+					if err != nil {
+						panic(err)
+					}
+					if err = eval.Rescale(ct, ct); err != nil {
+						panic(err)
+					}
+					ctx.evalPool.Put(eval)
+					
+					cOuts[no][ni*layer.KernelSize+k] = ct
+				}(no, ni, k)
 			}
 		}
 	}
+	wg.Wait()
 
+	wg.Add(numOut)
+	for no := 0; no < numOut; no++ {
+		go func(no int) {
+			defer wg.Done()
+			opOut.data[no] = ctx.AddMany(cOuts[no])
 
+			partialRot := make([]*rlwe.Ciphertext, 8)
+			rg := sync.WaitGroup{}
+			for rot := 1; rot < chIn; rot *= 8 {
+				rg.Add(8)
+				go func(rot int) {
+					defer rg.Done()
+					for i := 0; i < 8; i++ {
+						if rot*i < chIn {
+							if partialRot[i], err = ctx.RotationNew(opOut.data[no], rot*i*space); err != nil {
+								panic(err)
+							}
+						}
+					}
+				}(rot)
+				rg.Wait()
 
+				opOut.data[no] = ctx.AddMany(partialRot)
+			}
+			
+			vBias := make([]float64, 0)
+			for o := 0; o < chOut / numOut; o++ {
+				outIdx := no * chOut / numOut + o
 
+				bias := []float64{layer.Bias[outIdx]}
+				bias = append(bias, repeatZeros(opOut.interval - 1)...)
+				bias = repeatSlice(bias, opOut.size)
+				bias = append(bias, repeatZeros(space*(chIn/numIn) - len(bias))...)
+				vBias = append(vBias, bias...)				
+			}
+			vBias = repeatSlice(vBias, slots / len(vBias))
+
+			eval := ctx.evalPool.Get().(*hefloat.Evaluator)
+			eval.Add(opOut.data[no], vBias, opOut.data[no])
+			ctx.evalPool.Put(eval)
+		}(no)
+	}
+	wg.Wait()
 
 	return opOut
 }
